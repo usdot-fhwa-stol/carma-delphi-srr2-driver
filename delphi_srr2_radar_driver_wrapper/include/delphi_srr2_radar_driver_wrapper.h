cdaca158bfd0f0a642d2dda46f7da953d52c8bd7
qswawrq <qswawrq@gmail.com>
2019-01-24 11:53:40 -0800

    add radar pubs and subs

--- delphi_srr2_radar_driver_wrapper/include/delphi_srr2_radar_driver_wrapper.h
+++ delphi_srr2_radar_driver_wrapper/include/delphi_srr2_radar_driver_wrapper.h
@@ -17,11 +17,29 @@
  */
 
 #include <driver_wrapper/driver_wrapper.h>
+#include <delphi_srr_msgs/SrrStatus1.h>
+#include <delphi_srr_msgs/SrrStatus2.h>
 #include <delphi_srr_msgs/SrrStatus5.h>
+#include <radar_msgs/RadarDetectionArray.h>
 
 class DelphiSrr2RadarDriverWrapper : public cav::DriverWrapper
 {
-    ros::Subscriber srr_status5_sub_;
+    ros::Subscriber status1_sub_;
+    ros::Subscriber status2_sub_;
+    ros::Subscriber status5_sub_;
+    ros::Subscriber detection_sub_;
+    ros::Publisher  object_track_pub_;
+    ros::Publisher  radar_status_pub_;
+
+    delphi_srr_msgs::SrrStatus1ConstPtr status1_msg_;
+    delphi_srr_msgs::SrrStatus2ConstPtr status2_msg_;
+    delphi_srr_msgs::SrrStatus5ConstPtr status5_msg_;
+    radar_msgs::RadarDetectionArrayConstPtr track_msg_;
+
+    ros::Time last_update_time;
+
+    std::string sensor_frame_;
+    double bounding_box_size_;
 
 public:
     DelphiSrr2RadarDriverWrapper(int argc, char **argv, const std::string &name = "delphi_srr2_radar_driver_wrapper");
@@ -30,9 +48,27 @@ public:
 private:
 
     /**
-     * @brief Callback for handling srr_status5 message
+     * @brief Callback for handling srr_status message
      */
-    void srr_status5_cb(const delphi_srr_msgs::SrrStatus5ConstPtr& msg);
+    void status1_cb(const delphi_srr_msgs::SrrStatus1ConstPtr& msg);
+    void status2_cb(const delphi_srr_msgs::SrrStatus2ConstPtr& msg);
+    void status5_cb(const delphi_srr_msgs::SrrStatus5ConstPtr& msg);
+
+    /**
+     * @brief Callback for handling radar detection message
+     */
+    void detection_cb(const radar_msgs::RadarDetectionArrayConstPtr& msg);
+
+    /**
+     * @brief Publishes radar updates received from the vendor driver
+     */
+    void publish_object_track();
+    void publish_radar_status();
+
+    /**
+     * @brief Check radar data output timeout
+     */
+    void checkRadarTimeout();
 
     //cav::DriverWrapper members
     virtual void initialize();
--- delphi_srr2_radar_driver_wrapper/src/delphi_srr2_radar_driver_wrapper.cpp
+++ delphi_srr2_radar_driver_wrapper/src/delphi_srr2_radar_driver_wrapper.cpp
@@ -15,24 +15,59 @@
  */
 
 #include "delphi_srr2_radar_driver_wrapper.h"
+#include <radar_msgs/RadarTrackArray.h>
+#include <radar_msgs/RadarStatus.h>
 
 DelphiSrr2RadarDriverWrapper::DelphiSrr2RadarDriverWrapper(int argc, char **argv, const std::string &name) : DriverWrapper (argc, argv, name) {}
 
 DelphiSrr2RadarDriverWrapper::~DelphiSrr2RadarDriverWrapper() {}
 
 void DelphiSrr2RadarDriverWrapper::initialize() {
-    srr_status5_sub_ = nh_->subscribe("srr_status5", 10, &DelphiSrr2RadarDriverWrapper::srr_status5_cb, this);
+
+    // Set driver type
     status_.sensor = true;
+
+    // Initilize all subscribers for SRR driver
+    detection_sub_ = nh_->subscribe("detections", 10, &DelphiSrr2RadarDriverWrapper::detection_cb, this);
+    status1_sub_ = nh_->subscribe("srr_status1", 10, &DelphiSrr2RadarDriverWrapper::status1_cb, this);
+    status2_sub_ = nh_->subscribe("srr_status2", 10, &DelphiSrr2RadarDriverWrapper::status2_cb, this);
+    status5_sub_ = nh_->subscribe("srr_status5", 10, &DelphiSrr2RadarDriverWrapper::status5_cb, this);
+
+    // Initilize all publishers for CARMA
+    object_track_pub_ = nh_->advertise<radar_msgs::RadarTrackArray>("radar/srr/tracks_raw", 1);
+    radar_status_pub_ = nh_->advertise<radar_msgs::RadarStatus>("radar/srr/status", 1);
+
+    private_nh_->param<std::string>("sensor_frame", sensor_frame_, "srr2");
+    private_nh_->param<double>("bounding_box_size", bounding_box_size_, 5);
 }
 
-void DelphiSrr2RadarDriverWrapper::pre_spin() {}
+void DelphiSrr2RadarDriverWrapper::pre_spin()
+{
+    checkRadarTimeout();
+    publish_radar_status();
+    publish_object_track();
+}
 
 void DelphiSrr2RadarDriverWrapper::post_spin() {}
 
 void DelphiSrr2RadarDriverWrapper::shutdown() {}
 
-void DelphiSrr2RadarDriverWrapper::srr_status5_cb(const delphi_srr_msgs::SrrStatus5ConstPtr &msg)
+void DelphiSrr2RadarDriverWrapper::status1_cb(const delphi_srr_msgs::SrrStatus1ConstPtr &msg)
 {
+    last_update_time = ros::Time::now();
+    status1_msg_ = msg;
+}
+
+void DelphiSrr2RadarDriverWrapper::status2_cb(const delphi_srr_msgs::SrrStatus2ConstPtr &msg)
+{
+    last_update_time = ros::Time::now();
+    status2_msg_ = msg;
+}
+
+void DelphiSrr2RadarDriverWrapper::status5_cb(const delphi_srr_msgs::SrrStatus5ConstPtr &msg)
+{
+    last_update_time = ros::Time::now();
+    status5_msg_ = msg;
     switch(msg->CAN_TX_SYSTEM_STATUS)
     {
         case delphi_srr_msgs::SrrStatus5::CAN_TX_SYSTEM_STATUS_Running:
@@ -48,5 +83,57 @@ void DelphiSrr2RadarDriverWrapper::srr_status5_cb(const delphi_srr_msgs::SrrStat
         default:
             status_.status = cav_msgs::DriverStatus::OFF;
             break;
+    }
+}
+
+void DelphiSrr2RadarDriverWrapper::detection_cb(const radar_msgs::RadarDetectionArrayConstPtr &msg) {
+    last_update_time = ros::Time::now();
+    track_msg_ = msg;
+}
+
+void DelphiSrr2RadarDriverWrapper::publish_object_track()
+{
+    radar_msgs::RadarTrackArray tracks;
+    tracks.header.stamp = ros::Time::now();
+    tracks.header.frame_id = sensor_frame_;
+    for(radar_msgs::RadarDetection object : track_msg_->detections)
+    {
+        radar_msgs::RadarTrack track;
+        track.track_id = object.detection_id;
+        geometry_msgs::Polygon bounding_box;
+        geometry_msgs::Point32 point1, point2, point3, point4;
+        point1.x = static_cast<float> (object.position.x + 0.5 * bounding_box_size_);
+        point1.y = static_cast<float> (object.position.y - 0.5 * bounding_box_size_);
+        point2.x = static_cast<float> (object.position.x - 0.5 * bounding_box_size_);
+        point2.y = static_cast<float> (object.position.y - 0.5 * bounding_box_size_);
+        point3.x = static_cast<float> (object.position.x + 0.5 * bounding_box_size_);
+        point3.y = static_cast<float> (object.position.y + 0.5 * bounding_box_size_);
+        point4.x = static_cast<float> (object.position.x - 0.5 * bounding_box_size_);
+        point4.y = static_cast<float> (object.position.y + 0.5 * bounding_box_size_);
+        std::vector<geometry_msgs::Point32> polygon_points{point1, point2, point3, point4};
+        bounding_box.points = polygon_points;
+        track.track_shape = bounding_box;
+        track.linear_velocity = object.velocity;
+        tracks.tracks.push_back(track);
+    }
+    object_track_pub_.publish(tracks);
+}
+
+void DelphiSrr2RadarDriverWrapper::publish_radar_status()
+{
+    radar_msgs::RadarStatus status;
+    status.header.stamp = ros::Time::now();
+    status.curvature = static_cast<short> (status1_msg_->CAN_TX_CURVATURE);
+    status.yaw_rate = status1_msg_->CAN_TX_YAW_RATE_CALC;
+    status.vehicle_speed = status1_msg_->CAN_TX_VEHICLE_SPEED_CALC;
+    status.temperature = static_cast<char> (status2_msg_->CAN_TX_TEMPERATURE);
+    radar_status_pub_.publish(status);
+}
+
+void DelphiSrr2RadarDriverWrapper::checkRadarTimeout()
+{
+    if(ros::Time::now() - last_update_time > ros::Duration(0.3))
+    {
+        status_.status = cav_msgs::DriverStatus::FAULT;
     }
 }
